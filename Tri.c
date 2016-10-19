37ff520bd0641a268d2d5048f8f73fe4217f18e1
smbct <buchet.s@laposte.net>
2016-10-19 21:53:26 +0200

    glpk added to compare

---
+++ Tri.c
@@ -0,0 +1,14 @@
+/**
+ * \file Tri.c
+ * \brief implémentation de fonctions de tri
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Tri.h"
+
+//------------------------------------------------------------------------------
+void trier(int nbVal, int* ind, int* val) {
+
+}
