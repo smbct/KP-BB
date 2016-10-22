bc12c38912d69eb9bee45afe8d50406e91f4bec7
smbct <buchet.s@laposte.net>
2016-10-22 23:33:50 +0200

    improved bound, not working

--- BranchBound.c
+++ BranchBound.c
@@ -40,7 +40,7 @@ void BranchAndBound(Solution* opt) {
         } else {
 
             // relaxation du problème
-            relaxation(&sol, &ind, 1, &relax, &realisable);
+            relaxation2(&sol, &ind, 1, &relax, &realisable);
 
             // la borne supérieure est initialisée
             if(sup == -1) {
@@ -98,6 +98,7 @@ void relaxation(Solution *sol, int* ind, int borne, int* sup, int* realisable) {
     int relax = sol->z;
     int capa = sol->residu;
 
+    // détermination de l'élément critique
     while(affecter && indVar < sol->pb->nbVar) {
 
         // ajout d'un objet à la solution tant que possible
@@ -132,20 +133,120 @@ void relaxation(Solution *sol, int* ind, int borne, int* sup, int* realisable) {
             if(indVar+1 < sol->pb->nbVar) {
                 U0 = (int)floor( (double)capa * ( (double)sol->pb->profit[indVar+1] / (double)sol->pb->poids[indVar+1] ) );
             }
-            if(indVar > 0) {
+            if(indVar > *ind) {
                 U1 = (int) floor( (double)sol->pb->profit[indVar] - ((double)sol->pb->poids[indVar] - (double)capa) * ((double)sol->pb->profit[indVar-1] / (double)sol->pb->poids[indVar-1]) );
             }
 
-            if(U0 != -1 && (U1 == -1 || U0 < U0)) {
-                relax += U0;
-            } else {
-                relax += U1;
-            }
+            relax += (U0 > U1 ? U0 : U1);
         }
 
     }
 
     *sup = relax;
+}
+
+
+//------------------------------------------------------------------------------
+void relaxation2(Solution* sol, int* ind, int borne, int* sup, int* realisable) {
+
+    *realisable = 0;
+
+    // relaxation linéaire du problème
+    int affecter = 1;
+
+    int indVar = *ind;
+
+    int relax = sol->z;
+    int capa = sol->residu;
+
+    // détermination de l'élément critique
+    while(affecter && indVar < sol->pb->nbVar) {
+
+        // ajout d'un objet à la solution tant que possible
+        if(capa >= sol->pb->poids[indVar]) {
+            capa -= sol->pb->poids[indVar];
+            relax += sol->pb->profit[indVar];
+            sol->valeur[indVar] = 1;
+            indVar ++;
+        } else { // arrêt dès que ça ne l'est plus
+            affecter = 0;
+        }
+    }
+
+    if(capa == 0) {
+        *realisable = 1;
+        sol->residu = 0;
+        *ind = sol->pb->nbVar;
+        sol->z = relax;
+    } else { // si la solution n'est pas réalisable, il faut annuler les changements et calculer les bornes
+
+        for(int i = *ind; i < indVar; i++) {
+            sol->valeur[i] = 0;
+        }
+
+        // calcule des bornes en imposant respectivement x_s = 1 et x_s = 0
+        // calcule des nouveaux éléments critiques correspondant aux nouvelles contraintes
+        int s_1 = *ind, s_0 = *ind;
+        int j = *ind;
+        int cs_0 = sol->residu, cs_1 = sol->residu-sol->pb->poids[indVar]; // les capacités résiduelles pour les nouveaux éléments critiques
+        int sommePs_0 = 0, sommePs_1 = sol->pb->profit[indVar];
+        int trouves1 = 0, trouves0 = 0;
+        while(!trouves0 && j < sol->pb->nbVar) {
+
+            if(cs_0 >= sol->pb->poids[j]) {
+                if(j != indVar) {
+                    cs_0 -= sol->pb->poids[j];
+                    sommePs_0 += sol->pb->profit[j];
+                    if(s_0 < sol->pb->nbVar-1 && s_0+1 != indVar) {
+                        s_0 ++;
+                    } else if(s_0 + 1 == indVar && s_0 < sol->pb->nbVar-2) {
+                        s_0 += 2;
+                    } else {
+                        trouves0 = 1;
+                    }
+                }
+            } else { // s_0 sera nécessairement >= à s_1
+                trouves0 = 1;
+            }
+
+            if(!trouves1 && cs_1 >= sol->pb->poids[j]) {
+                if(j != indVar) {
+                    cs_1 -= sol->pb->poids[j];
+                    sommePs_1 += sol->pb->profit[j];
+                }
+                if(s_1 < sol->pb->nbVar-1 && s_1+1 != indVar) {
+                    s_1 ++;
+                } else if(s_1 + 1 == indVar && s_1 < sol->pb->nbVar-2) {
+                    s_1 += 2;
+                } else {
+                    trouves0 = 1;
+                }
+            } else {
+                trouves1 = 1;
+            }
+
+            j ++;
+        }
+
+        printf("debut : %d, fin : %d, s0 : %d, s1 : %d, indvar : %d\n", *ind, sol->pb->nbVar-1, s_0, s_1, indVar);
+
+        int U0 = -1;
+        int U1 = -1;
+        if(s_0 >= *ind && s_0 < sol->pb->nbVar) {
+            U0 = sommePs_0 + (int)floor((double)cs_0 * ( (double)sol->pb->poids[s_0] / (double)sol->pb->profit[s_0] ) );
+        }
+        if(s_1 >= *ind && s_1 < sol->pb->nbVar) {
+            U1 = sommePs_1 + (int)floor((double)cs_1 * ( (double)sol->pb->poids[s_1] / (double)sol->pb->profit[s_1] ) );
+        }
+
+        relax = sol->z + (U0 > U1 ? U0 : U1);
+
+        // printf("relax meilleure : %d\n", relax);
+
+    }
+
+    *sup = relax;
+
 }
 
 //------------------------------------------------------------------------------
